133f848835b7847d9d945ef09ddd230ef90d5c97
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2024-01-04 22:14:02 -0300

    Add busca-cursos.php and Buscador class
    This commit adds the busca-cursos.php file and the Buscador class to the project. The busca-cursos.php file contains code to search for online programming courses using the Alura website. The Buscador class is responsible for making HTTP requests and parsing the HTML response to extract the course names.

---
+++ busca-cursos.php
@@ -0,0 +1,17 @@
+<?php
+
+use Alura\BuscadorDeCursos\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+require_once 'vendor/autoload.php';
+
+$client = new Client(['base_uri' => 'https://www.alura.com.br/'] );
+$crawler = new Crawler();
+
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as $curso) {
+    echo $curso . PHP_EOL;
+}
